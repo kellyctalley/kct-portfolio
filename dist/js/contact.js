e44603a8a0e231311ed65f98122d295cd48d3cdc
vagrant <vagrant@precise32.(none)>
2015-10-26 20:12:10 +0000

    Working on contact form

--- dist/js/contact.js
+++ dist/js/contact.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
+
+    $("#contact-form").click(function() {
+        $("#name-error").empty();
+        $("#email-error").empty();
+        $("#message-error").empty();
+    });
+
     $("#contact-form-submit").click(function (e) {
-         e.preventDefault(); // prevent page reload
+        e.preventDefault(); // prevent page reload
         var formName = document.forms["contact-form"]["name"].value;
         var formEmail = document.forms["contact-form"]["email"].value;
         var formMessage = document.forms["contact-form"]["message"].value;
         var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+
         if (formName == null || formName == "") {
             $("#name-error").html("Please enter your name");
             return false;
